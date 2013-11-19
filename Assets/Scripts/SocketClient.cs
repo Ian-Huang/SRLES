6f24f29e7a3c097c21a946e74103bc65b22c3c84
Ian-Huang <dk781020@hotmail.com>
2013-11-19 19:09:32 +0800

    First Building at 2013/11/19

---
+++ Assets/Scripts/SocketClient.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System;
+using System.Text;
+
+public class SocketClient : MonoBehaviour
+{
+    public int bufferSize;
+    public string TestString;
+    public Rect rect;
+
+    public string IP = "127.0.0.1";
+    public int Port = 36000;
+
+    //語音程式的Process
+    private Process process;
+
+    byte[] myBufferBytes = new byte[1024];
+
+
+    //宣告網路資料流變數
+    private NetworkStream myNetworkStream;
+    //宣告 Tcp 用戶端物件
+    private TcpClient myTcpClient;
+
+    void Awake()
+    {
+        this.process = new Process();
+        this.process.StartInfo.FileName = @"D:\Practice folder\Socket Test\SRLES\SocketServer\Debug\SocketServer.exe";
+        this.process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+        this.process.Start();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.ServerConnect();
+        this.ReadDataInit();
+    }
+
+    void ServerConnect()
+    {
+        //建立 TcpClient 物件
+        this.myTcpClient = new TcpClient();
+        try
+        {
+            //測試連線至遠端主機
+            this.myTcpClient.Connect(this.IP, this.Port);
+            print("連線成功 !!\n");
+            this.myNetworkStream = myTcpClient.GetStream();
+        }
+        catch (Exception e)
+        {
+            print(e.Message);
+            print("主機" + this.IP + " 通訊埠 " + this.Port.ToString() + " 無法連接  !!");
+            return;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ReadDataInit()
+    {
+        this.myNetworkStream.BeginRead(this.myBufferBytes, 0, 1024, new AsyncCallback(this.EndReadCallback), null);
+    }
+
+    private void EndReadCallback(IAsyncResult ar)
+    {
+        int bytesRead = this.myNetworkStream.EndRead(ar);
+        string reString = Encoding.ASCII.GetString(this.myBufferBytes, 0, bytesRead);
+        print(reString);
+        this.ReadDataInit();
+    }
+
+
+    void OnGUI()
+    {
+
+        this.TestString = GUI.TextField(new Rect(50, 50, 200, 50), this.TestString);
+        if (GUI.Button(this.rect, "發送訊息"))
+        {
+            Byte[] myBytes = Encoding.ASCII.GetBytes(this.TestString);
+            this.myNetworkStream.Write(myBytes, 0, myBytes.Length);
+        }
+    }
+
+    /// <summary>
+    /// 結束程式時，關閉語音辨識伺服器
+    /// </summary>
+    void OnApplicationQuit()
+    {
+        this.process.CloseMainWindow(); //關閉
+
+    }
+}
---
+++ SocketServer/ServerProgram.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    class ServerProgram
+    {
+        public static void Main(string[] args)
+        {
+            bool closed = false;
+            byte[] testByte = new byte[1];
+
+            System.Net.IPAddress theIPAddress;
+            //建立 IPAddress 物件(本機)
+            theIPAddress = System.Net.IPAddress.Parse("127.0.0.1");
+
+            //建立監聽物件
+            TcpListener myTcpListener = new TcpListener(theIPAddress, 36000);
+            //啟動監聽
+            myTcpListener.Start();
+            Console.WriteLine("通訊埠 36000 等待用戶端連線...... !!");
+            Socket mySocket = myTcpListener.AcceptSocket();
+            do
+            {
+                try
+                {
+                    //偵測是否有來自用戶端的連線要求，若是
+                    //用戶端請求連線成功，就會秀出訊息。
+                    //if (mySocket.Connected)
+                    //{
+
+
+                    try
+                    {
+                        //使用Peek測試連線是否仍存在
+                        if (mySocket.Connected && mySocket.Poll(0, SelectMode.SelectRead))
+                            closed = (mySocket.Receive(testByte, SocketFlags.Peek) == 0);
+                    }
+                    catch (SocketException se)
+                    {
+                        closed = true;
+                    }
+                    if (!closed)
+                    {
+                        int dataLength;
+                        Console.WriteLine("連線成功 !!");
+                        byte[] myBufferBytes = new byte[1000];
+                        //取得用戶端寫入的資料
+                        dataLength = mySocket.Receive(myBufferBytes);
+
+                        Console.WriteLine("接收到的資料長度 {0} \n ", dataLength.ToString());
+                        Console.WriteLine("取出用戶端寫入網路資料流的資料內容 :");
+                        Console.WriteLine(Encoding.ASCII.GetString(myBufferBytes, 0, dataLength) + "\n");
+                        
+                        //將接收到的資料回傳給用戶端
+                        int si = mySocket.Send(myBufferBytes, dataLength, 0);
+                    }
+                    else
+                    {
+                        mySocket.Close();
+                        break;
+                    }
+                    Thread.Sleep(20);
+                    //}
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    mySocket.Close();
+                    break;
+                }
+            } while (true);
+            Console.WriteLine("結束連線");
+            Console.Read();        
+        }
+    }
+}
