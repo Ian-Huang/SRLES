e602bc3d11853cbc4d73e389e1a580c5f6508800
Ian-Huang <dk781020@hotmail.com>
2014-02-10 04:25:13 +0800

    AB 功能測試

---
+++ Assets/Scripts/ABtest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ABtest : MonoBehaviour
+{
+    public GameObject testObject;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadAB());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator LoadAB()
+    {
+        print(@"file:///D:\Github\SRLES\all.assetBunldes");
+        WWW www = new WWW(@"file:///D:\Github\SRLES\all.assetBunldes");
+
+        yield return www;
+        print("OK!!");
+        print(www.size);
+        AssetBundle asset = www.assetBundle;
+        print(www.assetBundle.LoadAll().Length);
+
+        Texture tt = asset.Load("Apple") as Texture;
+
+        this.testObject.renderer.material.mainTexture = tt;
+
+
+
+    }
+}
---
+++ Assets/Scripts/CreateAssetBunldes.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBunldes : MonoBehaviour
+{
+
+    [MenuItem("Custom Editor/Create AssetBunldes")]
+    static void ExecCreateAssetBunldes()
+    {
+       
+        // AssetBundle 的資料夾名稱及副檔名
+        string targetDir = "_AssetBunldes";
+        string extensionName = ".assetBunldes";
+
+        //取得在 Project 視窗中選擇的資源(包含資料夾的子目錄中的資源)
+        Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        //建立存放 AssetBundle 的資料夾
+        if (!Directory.Exists(targetDir)) Directory.CreateDirectory(targetDir);
+
+        foreach (Object obj in SelectedAsset)
+        {
+
+            Debug.Log("Create AssetBunldes name :" + obj);
+
+        }
+
+
+        string targetPath = targetDir + Path.DirectorySeparatorChar + "all" + extensionName;
+  
+        //这里注意第二个参数就行
+
+        if (BuildPipeline.BuildAssetBundle(null, SelectedAsset, targetPath, BuildAssetBundleOptions.CollectDependencies))
+        {
+
+            AssetDatabase.Refresh();
+
+        }
+        else
+        {
+
+
+
+        }
+
+    }
+}
