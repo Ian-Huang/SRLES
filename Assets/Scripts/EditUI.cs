2f99889eb35fed60cdb56fea2c6bd5aa9a1f7c60
Ian-Huang <dk781020@hotmail.com>
2014-02-16 00:04:39 +0800

    編輯介面UI - 課程資訊與單字庫連接基本架構50%

--- Assets/Scripts/EditUI.cs
+++ Assets/Scripts/EditUI.cs
@@ -8,8 +8,9 @@ public class EditUI : MonoBehaviour
     public GUISkin skin;
 
     //課程清單位置(之後要寫成彈性化相容於各種解析度)
-    public Rect WordDataWindowRect = new Rect(350, 20, 250, 250);
-    public Rect ClassListWindowRect = new Rect(20, 20, 250, 250);
+    public Rect WordDataWindowRect = new Rect(575, 20, 250, 250);
+    public Rect ClassInfoWindowRect = new Rect(300, 20, 250, 250);
+    public Rect ClassListWindowRect = new Rect(25, 20, 250, 250);
     public Rect HintWindowRect = new Rect(20, 20, 120, 50);
 
     public Vector2 ClassListScrollViewPosition; //課程清單滾輪位置
@@ -20,16 +21,25 @@ public class EditUI : MonoBehaviour
     public Dictionary<Object, bool> WordDataTogglesMap = new Dictionary<Object, bool>();   //單字庫對照表
     public string WordSearchString = "";        //單字庫搜尋列字串
 
+    public Vector2 ClassInfoScrollViewPosition; //課程資訊滾輪位置
+    public Dictionary<Object, bool> ClassInfoTogglesMap = new Dictionary<Object, bool>();   //課程資訊對照表
+    public string ClassNameString = "";         //課程名稱修改用字串
+
     void Start()
     {
         StartCoroutine(this.CreateWordDataToggleMap());
     }
 
+    /// <summary>
+    /// 生成單字庫映照表
+    /// </summary>
+    /// <returns></returns>
     IEnumerator CreateWordDataToggleMap()
     {
-        yield return ABTextureManager.script;
-        yield return !ABTextureManager.script.ABisFinish;
+        yield return ABTextureManager.script;   //確認ABTextureManager script是否存在  
+        yield return !ABTextureManager.script.ABisFinish;    //確認ABTextureManager AB資源是否已經載入完成
 
+        //將AB所有的圖檔載入致單字庫映照表
         foreach (var temp in ABTextureManager.script.TextureCollection)
             this.WordDataTogglesMap.Add(temp, false);
     }
@@ -44,8 +54,47 @@ public class EditUI : MonoBehaviour
         //單字庫視窗
         this.WordDataWindowRect = GUILayout.Window((int)WindowID.WordDataWindow, this.WordDataWindowRect, this.WordDataWindow, "單字庫");
 
+        //課程資訊視窗
+        this.ClassInfoWindowRect = GUILayout.Window((int)WindowID.ClassInfoWindow, this.ClassInfoWindowRect, this.ClassInfoWindow, "課程資訊");
+
         //提醒視窗
         //this.HintWindowRect = GUILayout.Window(1, this.HintWindowRect, this.HintWindow, "提醒");
+    }
+
+    /// <summary>
+    /// 課程資訊視窗
+    /// </summary>
+    /// <param name="windowID"></param>
+    void ClassInfoWindow(int windowID)
+    {
+        //-------將目前編輯的課程重新命名-------
+        GUILayout.BeginHorizontal();
+        {
+            this.ClassNameString = GUILayout.TextField(this.ClassNameString, 10);     //課程名稱修改用字串          
+            if (GUILayout.Button("修改課程名", GUILayout.ExpandWidth(false)))
+            {
+                //待補，未完成
+            }
+        }
+        GUILayout.EndHorizontal();
+        //-------將目前編輯的課程重新命名-------
+
+        this.ClassInfoScrollViewPosition = GUILayout.BeginScrollView(this.ClassInfoScrollViewPosition);
+        {
+            if (ABTextureManager.script)    //確認ABTextureManager script是否存在        
+                if (!ABTextureManager.script.ABisFinish)    //確認ABTextureManager AB資源是否已經載入完成
+                {
+                    Dictionary<Object, bool> tempDir = new Dictionary<Object, bool>(this.ClassInfoTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
+                    foreach (var key in tempDir.Keys)
+                    {
+                        //------單字庫一個單字的架構------                        
+                        GUIContent content = new GUIContent(key.name, key as Texture);  //圖+文字內容
+                        this.ClassInfoTogglesMap[key] = GUILayout.Toggle(this.ClassInfoTogglesMap[key], content, GUILayout.Height(50));   //因圖片太大，須設定大小                           
+                        //------單字庫一個單字的架構------
+                    }
+                }
+        }
+        GUILayout.EndScrollView();
     }
 
     /// <summary>
@@ -54,17 +103,19 @@ public class EditUI : MonoBehaviour
     /// <param name="windowID"></param>
     void WordDataWindow(int windowID)
     {
+        //------單字搜尋------
         GUILayout.BeginHorizontal();
         {
             this.WordSearchString = GUILayout.TextField(this.WordSearchString, 15);     //單字搜尋輸入列            
-            GUILayout.Box("Search", GUILayout.ExpandWidth(false));
+            GUILayout.Box("搜尋", GUILayout.ExpandWidth(false));
         }
         GUILayout.EndHorizontal();
+        //------單字搜尋------
 
         this.WordDataScrollViewPosition = GUILayout.BeginScrollView(this.WordDataScrollViewPosition);
         {
-            if (ABTextureManager.script)    //此句。測試時使用，正式版可以刪除            
-                if (!ABTextureManager.script.ABisFinish)
+            if (ABTextureManager.script)    //確認ABTextureManager script是否存在        
+                if (!ABTextureManager.script.ABisFinish)    //確認ABTextureManager AB資源是否已經載入完成
                 {
                     Dictionary<Object, bool> tempDir = new Dictionary<Object, bool>(this.WordDataTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
                     foreach (var key in tempDir.Keys)
@@ -80,6 +131,24 @@ public class EditUI : MonoBehaviour
                 }
         }
         GUILayout.EndScrollView();
+
+        //將在單字庫所選的單字新增至目前編輯的課程
+        if (GUILayout.Button("新增至課程", GUILayout.ExpandWidth(false)))
+        {
+            Dictionary<Object, bool> tempDir = new Dictionary<Object, bool>(this.WordDataTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
+
+            //確認對照表，將被勾選的物件新增至課程
+            foreach (var key in tempDir.Keys)
+            {
+                if (this.WordDataTogglesMap[key])
+                {
+                    if (!this.ClassInfoTogglesMap.ContainsKey(key))     //確認目前的課程是否已經有選擇的單字
+                        this.ClassInfoTogglesMap.Add(key, false);
+                    //將單字庫被選擇的狀態取消
+                    this.WordDataTogglesMap[key] = false;
+                }
+            }
+        }
     }
 
     /// <summary>
@@ -124,7 +193,6 @@ public class EditUI : MonoBehaviour
             if (GUILayout.Button("刪除"))
             {
                 Dictionary<string, bool> tempDir = new Dictionary<string, bool>(this.ClassListTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
-
                 //確認對照表，將被勾選的物件刪除
                 foreach (var key in tempDir.Keys)
                 {
@@ -171,7 +239,7 @@ public class EditUI : MonoBehaviour
 
     public enum WindowID
     {
-        ClassListWindow = 0, WordDataWindow = 1,
+        ClassListWindow = 0, WordDataWindow = 1, ClassInfoWindow = 2,
         ClassListDeleteHint = 100
     }
 }
