2e545f9b0f8c3aca165009116b891a960e67fa50
Ian-Huang <dk781020@hotmail.com>
2014-02-15 18:04:06 +0800

    編輯介面UI - 單字庫UI，基本架構完成

--- Assets/Scripts/EditUI.cs
+++ Assets/Scripts/EditUI.cs
@@ -5,21 +5,38 @@ using System.Collections.Generic;
 [ExecuteInEditMode]
 public class EditUI : MonoBehaviour
 {
+    public GUISkin skin;
+
     //課程清單位置(之後要寫成彈性化相容於各種解析度)
     public Rect WordDataWindowRect = new Rect(350, 20, 250, 250);
     public Rect ClassListWindowRect = new Rect(20, 20, 250, 250);
     public Rect HintWindowRect = new Rect(20, 20, 120, 50);
 
     public Vector2 ClassListScrollViewPosition; //課程清單滾輪位置
-    public Dictionary<string, bool> ClassTogglesMap = new Dictionary<string, bool>();   //課程清單對照表
+    public Dictionary<string, bool> ClassListTogglesMap = new Dictionary<string, bool>();   //課程清單對照表
     public int CreateClassCount = 1;    //課程清單產生次數(避免重複命名使用)
 
-    public string stringToEdit = "Hello World";
-    public Vector2 WordDataScrollViewPosition; //單字庫滾輪位置
+    public Vector2 WordDataScrollViewPosition;  //單字庫滾輪位置
+    public Dictionary<Object, bool> WordDataTogglesMap = new Dictionary<Object, bool>();   //單字庫對照表
+    public string WordSearchString = "";        //單字庫搜尋列字串
+
+    void Start()
+    {
+        StartCoroutine(this.CreateWordDataToggleMap());
+    }
+
+    IEnumerator CreateWordDataToggleMap()
+    {
+        yield return ABTextureManager.script;
+        yield return !ABTextureManager.script.ABisFinish;
+
+        foreach (var temp in ABTextureManager.script.TextureCollection)
+            this.WordDataTogglesMap.Add(temp, false);
+    }
 
     void OnGUI()
     {
-        //GUI.skin.window.fontSize = 16;
+        GUI.skin = this.skin;
 
         //課程清單視窗
         this.ClassListWindowRect = GUILayout.Window((int)WindowID.ClassListWindow, this.ClassListWindowRect, this.ClassListWindow, "課程清單");
@@ -37,21 +54,30 @@ public class EditUI : MonoBehaviour
     /// <param name="windowID"></param>
     void WordDataWindow(int windowID)
     {
-        this.stringToEdit = GUILayout.TextField(this.stringToEdit, 25);
+        GUILayout.BeginHorizontal();
+        {
+            this.WordSearchString = GUILayout.TextField(this.WordSearchString, 15);     //單字搜尋輸入列            
+            GUILayout.Box("Search", GUILayout.ExpandWidth(false));
+        }
+        GUILayout.EndHorizontal();
 
         this.WordDataScrollViewPosition = GUILayout.BeginScrollView(this.WordDataScrollViewPosition);
         {
-            if (ABTextureManager.script)    //此句。測試時使用，正式版可以刪除
-            {
+            if (ABTextureManager.script)    //此句。測試時使用，正式版可以刪除            
                 if (!ABTextureManager.script.ABisFinish)
                 {
-                    foreach (var temp in ABTextureManager.script.TextureCollection)
+                    Dictionary<Object, bool> tempDir = new Dictionary<Object, bool>(this.WordDataTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
+                    foreach (var key in tempDir.Keys)
                     {
-                        GUILayout.Box(temp.name);
+                        if (key.name.StartsWith(this.WordSearchString, System.StringComparison.OrdinalIgnoreCase))  //將搜尋列輸入的字串與單字庫各單字開頭進行比較，以方便進行搜尋
+                        {
+                            //------單字庫一個單字的架構------                        
+                            GUIContent content = new GUIContent(key.name, key as Texture);  //圖+文字內容
+                            this.WordDataTogglesMap[key] = GUILayout.Toggle(this.WordDataTogglesMap[key], content, GUILayout.Height(50));   //因圖片太大，須設定大小                           
+                            //------單字庫一個單字的架構------
+                        }
                     }
-
                 }
-            }
         }
         GUILayout.EndScrollView();
     }
@@ -64,14 +90,14 @@ public class EditUI : MonoBehaviour
     {
         this.ClassListScrollViewPosition = GUILayout.BeginScrollView(this.ClassListScrollViewPosition);
         {
-            Dictionary<string, bool> tempDir = new Dictionary<string, bool>(this.ClassTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
+            Dictionary<string, bool> tempDir = new Dictionary<string, bool>(this.ClassListTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
 
             foreach (var key in tempDir.Keys)
             {
                 //------一個課程的架構------
                 GUILayout.BeginHorizontal();
                 {
-                    this.ClassTogglesMap[key] = GUILayout.Toggle(this.ClassTogglesMap[key], key);
+                    this.ClassListTogglesMap[key] = GUILayout.Toggle(this.ClassListTogglesMap[key], key);
                     GUILayout.FlexibleSpace();
                     GUILayout.Button("編輯");
                 }
@@ -87,22 +113,23 @@ public class EditUI : MonoBehaviour
             if (GUILayout.Button("新增"))
             {
                 //確認是否有重複命名問題
-                while (this.ClassTogglesMap.ContainsKey("未命名課程" + this.CreateClassCount))
+                while (this.ClassListTogglesMap.ContainsKey("未命名課程" + this.CreateClassCount))
                     this.CreateClassCount++;
 
                 //將新增課程至對照表
-                this.ClassTogglesMap.Add("未命名課程" + this.CreateClassCount, false);
+                this.ClassListTogglesMap.Add("未命名課程" + this.CreateClassCount, false);
                 //將清單滾輪移動至最下方
                 this.ClassListScrollViewPosition.y = int.MaxValue;
             }
             if (GUILayout.Button("刪除"))
             {
-                Dictionary<string, bool> tempDir = new Dictionary<string, bool>(this.ClassTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
+                Dictionary<string, bool> tempDir = new Dictionary<string, bool>(this.ClassListTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
 
+                //確認對照表，將被勾選的物件刪除
                 foreach (var key in tempDir.Keys)
                 {
-                    if (this.ClassTogglesMap[key])
-                        this.ClassTogglesMap.Remove(key);
+                    if (this.ClassListTogglesMap[key])
+                        this.ClassListTogglesMap.Remove(key);
                 }
             }
             //-------課程清單"新增"、"刪除"兩個Button設置-------
