023792575ed96105b529b3fc5e71439c6c42629b
Ian-Huang <dk781020@hotmail.com>
2014-02-25 17:13:47 +0800

    Update

---
+++ Assets/Scripts/UI/ReadyGameUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+[ExecuteInEditMode]
+public class ReadyGameUI : MonoBehaviour
+{
+    public GUISkin skin;
+
+
+    void Start()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = this.skin;
+    }
+
+
+    public enum WindowID
+    {
+
+    }
+}
