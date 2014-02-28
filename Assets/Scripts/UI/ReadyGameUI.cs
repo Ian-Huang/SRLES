b52820808707a4d569229c8710efa5b06930a865
Ian-Huang <dk781020@hotmail.com>
2014-02-28 14:23:42 +0800

    Update

--- Assets/Scripts/UI/ReadyGameUI.cs
+++ Assets/Scripts/UI/ReadyGameUI.cs
@@ -10,11 +10,17 @@ public class ReadyGameUI : MonoBehaviour
 
     //課程清單位置(之後要寫成彈性化相容於各種解析度)
     public Rect ClassListWindowRect = new Rect(25, 20, 250, 250);
+    public Rect ClassInfoWindowRect = new Rect(300, 20, 250, 250);
+
 
     public Vector2 ClassListScrollViewPosition; //課程清單滾輪位置
     public Dictionary<string, bool> ClassListTogglesMap = new Dictionary<string, bool>();   //課程清單對照表
-    public int ChooseClassIndex;    //目前被選擇的課程序號
+    private int ChooseClassIndex = -1;    //目前被選擇的課程序號
 
+    public Vector2 ClassInfoScrollViewPosition; //課程資訊滾輪位置
+    public Dictionary<Object, bool> ClassInfoTogglesMap = new Dictionary<Object, bool>();   //課程資訊對照表
+    public string ModifyClassNameString;        //課程名稱修改用字串
+    public string CurrentEditClassName;         //當前選擇編輯的課程名稱
 
 
     void Start()
@@ -45,6 +51,37 @@ public class ReadyGameUI : MonoBehaviour
         //課程清單視窗
         this.ClassListWindowRect = GUILayout.Window((int)WindowID.ClassListWindow, this.ClassListWindowRect, this.ClassListWindow, "課程清單");
 
+        //課程資訊視窗
+        this.ClassInfoWindowRect = GUILayout.Window((int)WindowID.ClassInfoWindow, this.ClassInfoWindowRect, this.ClassInfoWindow, this.CurrentEditClassName);
+
+
+    }
+
+    /// <summary>
+    /// 課程資訊視窗
+    /// </summary>
+    /// <param name="windowID"></param>
+    void ClassInfoWindow(int windowID)
+    {
+
+        this.ClassInfoScrollViewPosition = GUILayout.BeginScrollView(this.ClassInfoScrollViewPosition);
+        {
+            if (ABTextureManager.script)    //確認ABTextureManager script是否存在        
+                if (ABTextureManager.script.ABRoadFinish)    //確認ABTextureManager AB資源是否已經載入完成
+                {
+                    Dictionary<Object, bool> tempDir = new Dictionary<Object, bool>(this.ClassInfoTogglesMap);  //不可直接對Dictionary做讀取，須建立一個暫存
+                    foreach (var key in tempDir.Keys)
+                    {
+                        //------單字庫一個單字的架構------                        
+                        GUIContent content = new GUIContent(key.name, key as Texture);  //圖+文字內容
+                        this.ClassInfoTogglesMap[key] = GUILayout.Toggle(this.ClassInfoTogglesMap[key], content, GUILayout.Height(70));   //因圖片太大，須設定大小                           
+                        //------單字庫一個單字的架構------
+                    }
+                }
+        }
+        GUILayout.EndScrollView();
+
+
     }
 
     /// <summary>
@@ -67,6 +104,6 @@ public class ReadyGameUI : MonoBehaviour
 
     public enum WindowID
     {
-        ClassListWindow = 0
+        ClassListWindow = 0, ClassInfoWindow = 2
     }
 }
