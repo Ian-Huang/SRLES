8d4833ebf8c018250486b3467df75d54f8990b0c
Ian-Huang <dk781020@hotmail.com>
2014-03-01 23:30:34 +0800

    Update 按鈕功能

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -3,22 +3,42 @@ using System.Collections;
 
 public class ButtonController : MonoBehaviour
 {
-    public ButtonManager.ButtonEvent ButtonEvent;
+    public ButtonEvent buttonEvent;
 
     void OnMouseDown()
     {
-        ButtonManager.script.ButtonDown(this.ButtonEvent);
+        this.ButtonDown(this.buttonEvent);
     }
 
-    // Use this for initialization
-    void Start()
+    void ButtonDown(ButtonEvent BtnEvent)
     {
-
+        switch (BtnEvent)
+        {
+            case ButtonEvent.EnterReadyGame:
+                Application.LoadLevelAsync("ReadyGame");
+                break;
+            case ButtonEvent.EnterEditMode:
+                Application.LoadLevelAsync("Edit");
+                break;
+            case ButtonEvent.EnterTrainMode:
+                Application.LoadLevelAsync("TrainMode");
+                break;
+            case ButtonEvent.EnterGameMode:
+                Application.LoadLevelAsync("Game");
+                break;
+            case ButtonEvent.EnterHome:
+                Application.LoadLevelAsync("Home");
+                break;
+            case ButtonEvent.ExitGame:
+                Application.Quit();
+                break;
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum ButtonEvent
     {
-
+        EnterReadyGame = 0, EnterEditMode = 1, ExitGame = 2, EnterGameMode = 3, EnterTrainMode = 4, EnterHome = 5
     }
 }
