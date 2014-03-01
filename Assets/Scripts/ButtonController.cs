3263d25dd205488ffc5220bb1298bc308929c112
Ian-Huang <dk781020@hotmail.com>
2014-03-01 18:06:55 +0800

    新增按鈕圖片、事件處理

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour
+{
+    public ButtonManager.ButtonEvent ButtonEvent;
+
+    void OnMouseDown()
+    {
+        ButtonManager.script.ButtonDown(this.ButtonEvent);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
