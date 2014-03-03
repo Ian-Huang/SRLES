32b40a67e4c67fe9a57d1aa6811609d347eec7b5
Ian-Huang <dk781020@hotmail.com>
2014-03-03 17:24:31 +0800

    Update

---
+++ Assets/Scripts/CardMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardMove : MonoBehaviour
+{
+    public Vector3 StartPoint;
+    public Vector3 RunPoint;
+    public Vector3 EndPoint;
+
+    public float RunTime;
+    public iTween.EaseType easeType;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = this.StartPoint;
+        iTween.MoveTo(this.gameObject, iTween.Hash("position", this.RunPoint, "time", this.RunTime, "easetype", this.easeType));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
