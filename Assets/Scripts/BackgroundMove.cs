b7ee1691814cda90c7903104d82a0f3d7c17bd57
Ian-Huang <dk781020@hotmail.com>
2014-03-17 01:47:36 +0800

    更改介面風格

---
+++ Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMove : MonoBehaviour
+{
+    public float MoveTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        iTween.ScaleTo(this.gameObject, iTween.Hash("x", 2.1f, "y", 2.1f, "time", this.MoveTime));
+        iTween.MoveTo(this.gameObject, iTween.Hash("x", 2.7f, "y", -0.67f, "time", this.MoveTime));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
